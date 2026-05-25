62e2d232bf6b8b0b25b308b2fb3af0d6b57a2c4f
A1Albrawe <168625325+A1Albrawe@users.noreply.github.com>
2026-05-26 00:52:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from home import home_blueprint
 from snake import snake_blueprint
 from tetris import tetris_blueprint
 from report import report_blueprint # استدعاء الملف المدمج والجاهز
+from admin import admin_blueprint
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.register_blueprint(home_blueprint)
 app.register_blueprint(snake_blueprint)
 app.register_blueprint(tetris_blueprint)
 app.register_blueprint(report_blueprint) # تسجيل مسارات التقرير والبوت المدمجة
+app.register_blueprint(admin_blueprint)
 
 handler = app
 
