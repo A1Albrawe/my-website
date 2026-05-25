10225f0a8e14f7be7ae84ce0a56ffa6265c2886f
A1Albrawe <168625325+A1Albrawe@users.noreply.github.com>
2026-05-25 22:27:38 +0300

    Add report blueprint for issue reporting

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from home import home_blueprint
 from snake import snake_blueprint
 from tetris import tetris_blueprint
+from report import report_blueprint # تم استيراد صفحة الإبلاغ عن المشاكل بنجاح
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(snake_blueprint)
 app.register_blueprint(tetris_blueprint)
+app.register_blueprint(report_blueprint) # تم تسجيل المسار البرمجي لصفحة المشاكل في الخادم
 
 if __name__ == '__main__':
     app.run()
