25c4e4372cca3ac60a0668b0ec5efbdd8cc7bc6e
ALBRAWE - البراوي <168625325+A1Albrawe@users.noreply.github.com>
2026-05-27 03:16:48 +0300

    Update index.py

--- netlify/functions/index.py
+++ netlify/functions/index.py
@@ -1,13 +1,10 @@
 import os
 import sys
 
-# لتضمين الملفات والمجلدات البرمجية الأساسية في المسار
+# إضافة المسار الرئيسي للمشروع لضمان قراءة باقي الملفات
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-# استيراد تطبيق فلاسك الخاص بك من ملف app.py أو index.py الرئيسي
-from app import app 
-
-# مكتبة تحويل Flask إلى Serverless متوافقة مع AWS Lambda / Netlify
+from app import app
 from serverless_wsgi import handle_request
 
 def handler(event, context):
