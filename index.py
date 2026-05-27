9e6a26efbd1b83a9bc87e40ec0aa2672e93eab2a
ALBRAWE - البراوي <168625325+A1Albrawe@users.noreply.github.com>
2026-05-27 16:33:03 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
+import sys
+import os
 
+# تأمين مسار المشروع الحالي لضمان قراءة كافة الـ Blueprints والحزم دون أخطاء استيراد
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# استدعاء متغير التطبيق النواة من ملف app.py
+from app import app
+
+# Vercel يتوقع استقبال المتغير باسم app ليقوم بالإقلاع السحابي الفوري
+app = app
